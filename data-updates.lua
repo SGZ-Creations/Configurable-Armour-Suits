e6592e7fe5875149afbbf7e159ef9ca75d14076b
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-03-12 20:26:10 +0100

    Changelog, data-updates
    Portion of changelog and all i know of data-update

---
+++ data-updates.lua
@@ -0,0 +1,20 @@
+{
+	data.raw["equipemnt-grid"]["modular-armour"].height = 5
+	data.raw["equipemnt-grid"]["modular-armour"].width = 5
+	data.raw["equipemnt-grid"]["power-armor"].height = 10
+	data.raw["equipemnt-grid"]["power-armor"].width = 10
+	data.raw["equipemnt-grid"]["power-armor-mk2"].hight = 20
+	data.raw["equipemnt-grid"]["power-armor-mk2"].width = 20
+	data.raw["equipemnt-grid"]["power-armor-mk3"].hight = 30
+	data.raw["equipemnt-grid"]["power-armor-mk3"].width = 30
+	data.raw["equipemnt-grid"]["power-armor-mk4"].hight = 40
+	data.raw["equipemnt-grid"]["power-armor-mk4"].width = 40
+	data.raw["equipemnt-grid"]["se-thruster-suie-grid"].height = 15
+	data.raw["equipemnt-grid"]["se-thruster-suie-grid"].width = 15
+	data.raw["equipemnt-grid"]["se-thruster-suie-2-grid"].height = 30
+	data.raw["equipemnt-grid"]["se-thruster-suie-2-grid"].width = 30
+	data.raw["equipemnt-grid"]["se-thruster-suie-3-grid"].height = 45
+	data.raw["equipemnt-grid"]["se-thruster-suie-3-grid"].width = 45
+	data.raw["equipemnt-grid"]["se-thruster-suie-4-grid"].height = 60
+	data.raw["equipemnt-grid"]["se-thruster-suie-4-grid"].width = 60
+},
