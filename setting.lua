5f7a3f53187c555feab4d19b4f11ba56c767b6e7
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-03-13 16:41:35 +0100

    start of settings work
    home page fix

--- setting.lua
+++ setting.lua
@@ -1,1 +1,14 @@
+data:extend({
+	{
+	type = "int-setting",
+	name = "armour-grid-size",
+	setting_type = "startup",
+	minimum_value = 1,
+	maximum_value = 9
+	deafault_value = {5, 5}
+	}
+})
 
+--if mods[bobswarefare] then
+
+--data.raw("modular-armor", 5, 5) = setting.startup 
