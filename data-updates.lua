3cfea668e8a5c547c9fd253126b3565491328e49
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-05 06:35:59 +0200

    duplicate fixed

--- data-updates.lua
+++ data-updates.lua
@@ -6,7 +6,7 @@ local function SetGridSize(grid_name, new_height, new_width)
 end
  
 	SetGridSize("modular-armor", settings.startup["modular-armour-grid-size-h"].value, settings.startup["modular-armour-grid-size-w"].value)
-	SetGridSize("power-armor", settings.startup["modular-armour-grid-size-h"].value, settings.startup["modular-armour-grid-size-w"].value)
+	SetGridSize("power-armor", settings.startup["power-armor-grid-size-h"].value, settings.startup["power-armor-grid-size-w"].value)
 	SetGridSize("power-armor-mk2", settings.startup["power-armor-mk2-grid-size-h"].value, settings.startup["power-armor-mk2-grid-size-w"].value)
 	SetGridSize("power-armor-mk3", settings.startup["power-armor-mk3-grid-size-h"].value, settings.startup["power-armor-mk3-grid-size-w"].value)
 	SetGridSize("power-armor-mk4",settings.startup["power-armor-mk4-grid-size-h"].value, settings.startup["power-armor-mk4-grid-size-w"].value)
