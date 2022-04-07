612390c9f607ddb37441e56d8c6c54cafb7e65de
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-07 16:58:31 +0200

    armour working ?

--- data-updates.lua
+++ data-updates.lua
@@ -5,6 +5,6 @@ local function SetGridSize(grid_name, new_height, new_width)
   end
 end
 
-	SetGridSize("modular-armor", settings.startup["modular-armor-grid-size-h"].value, settings.startup["modular-armor-grid-size-w"].value)
-	SetGridSize("power-armor", settings.startup["power-armor-grid-size-h"].value, settings.startup["power-armor-grid-size-w"].value)
-	SetGridSize("power-armor-mk2", settings.startup["power-armor-mk2-grid-size-h"].value, settings.startup["power-armor-mk2-grid-size-w"].value)
+	SetGridSize("smal-equipment-grid", settings.startup["modular-armor-grid-size-h"].value, settings.startup["modular-armor-grid-size-w"].value)
+	SetGridSize("medium-equipment-grid", settings.startup["power-armor-grid-size-h"].value, settings.startup["power-armor-grid-size-w"].value)
+	SetGridSize("large-equipment-grid", settings.startup["power-armor-mk2-grid-size-h"].value, settings.startup["power-armor-mk2-grid-size-w"].value)
