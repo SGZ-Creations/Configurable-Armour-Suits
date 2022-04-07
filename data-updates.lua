a8fda3f8acec60d3af13aa6749f04a6436e277aa
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-07 17:07:15 +0200

    small :p

--- data-updates.lua
+++ data-updates.lua
@@ -5,6 +5,6 @@ local function SetGridSize(grid_name, new_height, new_width)
   end
 end
 
-	SetGridSize("smal-equipment-grid", settings.startup["modular-armor-grid-size-h"].value, settings.startup["modular-armor-grid-size-w"].value)
+	SetGridSize("small-equipment-grid", settings.startup["modular-armor-grid-size-h"].value, settings.startup["modular-armor-grid-size-w"].value)
 	SetGridSize("medium-equipment-grid", settings.startup["power-armor-grid-size-h"].value, settings.startup["power-armor-grid-size-w"].value)
 	SetGridSize("large-equipment-grid", settings.startup["power-armor-mk2-grid-size-h"].value, settings.startup["power-armor-mk2-grid-size-w"].value)
