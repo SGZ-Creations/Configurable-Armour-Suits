3ed93f75039afc4ced5ce2df4b0236691989c1bf
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-07 09:09:23 +0200

    fixed few ","

--- settings.lua
+++ settings.lua
@@ -1,56 +1,56 @@
-data:extend({
+ data:extend({
 	{
 	type = "int-setting",
 	name = "modular-armor-grid-size-h",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 9
-	default_value = 5
+	minimum_value = 5,
+	maximum_value = 9,
+	default_value = 5,
 	order = "Aaa"
 	},
 	{
 	type = "int-setting",
 	name = "modular-armor-grid-size-w",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 9
-	default_value = 5
+	minimum_value = 5,
+	maximum_value = 9,
+	default_value = 5,
 	order = "Aab"
 	},
 	{
 	type = "int-setting",
 	name = "power-armor-grid-size-h",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 19
-	default_value = 10
+	minimum_value = 5,
+	maximum_value = 19,
+	default_value = 10,
 	order = "Aac"
 	},
 	{
 	type = "int-setting",
 	name = "power-armor-grid-size-w",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 19
-	default_value = 10
+	minimum_value = 5,
+	maximum_value = 19,
+	default_value = 10,
 	order = "Aad"
 	},
 	{
 	type = "int-setting",
 	name = "power-armor-mk2-grid-size-h",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 29
-	default_value = 20
+	minimum_value = 5,
+	maximum_value = 29,
+	default_value = 20,
 	order = "Aac"
 	},
 	{
 	type = "int-setting",
 	name = "power-armor-mk2-grid-size-w",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 29
-	default_value = 20
+	minimum_value = 5,
+	maximum_value = 29,
+	default_value = 20,
 	order = "Aad"
 	},
 	
@@ -59,9 +59,9 @@ data:extend({
 	type = "int-setting",
 	name = "power-armor-mk3-grid-size-h",
 	setting_type = "startup",
-	minimum_value = 5
-	maximum_value = 39
-	default_value = 30
+	minimum_value = 5,
+	maximum_value = 39,
+	default_value = 30,
 	order = "Caa"
 	},
 	{
@@ -205,7 +205,7 @@ end
 	default_value = 40
 	order = "Dad"
 	},
-	end
+end
 	
 	if mods["space-exploration"] then
 	{
@@ -302,4 +302,4 @@ end
 	order = "Fab"
 	},
 end
-})
+}),
