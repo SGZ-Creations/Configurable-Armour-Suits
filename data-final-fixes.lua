59076e0db62d7ba5a52f6869c49471cc9593907f
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-16 19:15:43 +0200

    final fix ?!
    line 8-10 removed on test build

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,14 +1,15 @@
-local function SetGridSize(grid_name, setting_name)
-	local width = settings.startup[setting_name.."-grid-size-w"].value
-	local height = settings.startup[setting_name.."-grid-size-h"].value
-	if data.raw["equipment-grid"][grid_name] then
-		data.raw["equipment-grid"][grid_name].width = width
-		data.raw["equipment-grid"][grid_name].height = height
+local function SetGridSize(grid_name, setting_name_prefix)
+	local width = settings.startup[setting_name_prefix.."-grid-size-w"].value
+	local height = settings.startup[setting_name_prefix.."-grid-size-h"].value
+	local grid = data.raw["equipment-grid"][grid_name]
+	if grid then
+		grid.width = width
+		grid.height = height
+	end
+	if grid.name == "se-thruster-suit-3-grid" then
+		error(grid.width .. ' ' .. grid.height)
 	end
 end
-
--- Try to follow correct indention for readability  either 2, 3, 4 is standard
--- anything under a control statement gets indented until it `end`s
 
 SetGridSize("small-equipment-grid", "modular-armor")
 SetGridSize("medium-equipment-grid", "power-armor")
@@ -17,7 +18,7 @@ SetGridSize("large-equipment-grid", "power-armor-mk2")
 if mods["space-exploration"] then
 	SetGridSize("se-thruster-suit-grid", "se-thruster-suit")
 	SetGridSize("se-thruster-suit-2-grid", "se-thruster-suit-2")
-    SetGridSize("se-thruster-suit-3-grid", "se-thruster-suit-3")
+	SetGridSize("se-thruster-suit-3-grid", "se-thruster-suit-3")
 	SetGridSize("se-thruster-suit-4-grid", "se-thruster-suit-4")
 end
 
