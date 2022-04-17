08af532bd91f1abbcc510c2d814ebf20d2943b72
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-04-17 10:01:44 +0200

    Progress in fixing K2 SE

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,9 +6,6 @@ local function SetGridSize(grid_name, setting_name_prefix)
 		grid.width = width
 		grid.height = height
 	end
-	if grid.name == "se-thruster-suit-3-grid" then
-		error(grid.width .. ' ' .. grid.height)
-	end
 end
 
 SetGridSize("small-equipment-grid", "modular-armor")
@@ -16,10 +13,17 @@ SetGridSize("medium-equipment-grid", "power-armor")
 SetGridSize("large-equipment-grid", "power-armor-mk2")
 
 if mods["space-exploration"] then
-	SetGridSize("se-thruster-suit-grid", "se-thruster-suit")
-	SetGridSize("se-thruster-suit-2-grid", "se-thruster-suit-2")
-	SetGridSize("se-thruster-suit-3-grid", "se-thruster-suit-3")
-	SetGridSize("se-thruster-suit-4-grid", "se-thruster-suit-4")
+	if mods["Krastorio2"] then
+		SetGridSize("medium-equipment-grid", "se-thruster-suit")
+		SetGridSize("large-equipment-grid", "se-thruster-suit-2")
+		SetGridSize("mk3-armor-grid", "se-thruster-suit-3")
+		SetGridSize("mk4-armor-grid", "se-thruster-suit-4")
+	else
+		SetGridSize("se-thruster-suit-grid", "se-thruster-suit")
+		SetGridSize("se-thruster-suit-2-grid", "se-thruster-suit-2")
+		SetGridSize("se-thruster-suit-3-grid", "se-thruster-suit-3")
+		SetGridSize("se-thruster-suit-4-grid", "se-thruster-suit-4")
+	end
 end
 
 if mods["custom-power-armor-fix"] then
@@ -34,9 +38,14 @@ if mods["bobwarfare"] then
     SetGridSize("power-armor-equipment-grid-mk5", "bob-power-armor-mk5")
 end
 
-if mods["Krastorio2"] then
-	SetGridSize("mk3-armor-grid", "mk3-armor")
-	SetGridSize("mk4-armor-grid", "mk4-armor")
+-- Currently K2 have been SE at current momment if i compare to erlier when i IT was weaird.
+-- Other than that i went to bed for toady as I'm over tired. if u solve the mixup i will greatly apriciate it 
+
+if mods["Krastorio2"] then -- only change K2 if not SE...
+	if not mods["space-exploration"] then
+		SetGridSize("mk3-armor-grid", "mk3-armor")
+		SetGridSize("mk4-armor-grid", "mk4-armor")
+	end
 end
 
 if mods["Power Armor MK3"] then
@@ -47,3 +56,8 @@ end
 if mods["RampantArsenal"] then
 	SetGridSize("power-armor-mk3-grid-rampant-arsenal", "power-armor-mk3-armor-rampant-arsenal")
 end
+
+-- if mods["PowerAndArmor"] then
+-- 
+--
+--end
