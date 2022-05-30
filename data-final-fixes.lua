e74908404839a549e2418c0c183be98e010f556a
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-05-30 05:44:31 +0200

    Fix attemt 3

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -7,9 +7,10 @@ local function SetGridSize(grid_name, setting_name_prefix)
 		grid.height = height
 	end
 end
+-- crash here if mods
 -- 1= Inv size prefix. 2= name of item. 3= add the: "-inv-size"
 local function SetInvSize(armor_name, setting_name_prefix)
-	data.raw.armor[armor_name].inventory_size_bonus = settings.startup[setting_name_prefix..armor_name.."-inv-size"].value
+	data.raw["armor"][armor_type].inventory_size_bonus = settings.startup[setting_name_prefix..armor_name.."-inv-size"].value
 end
 
 -- Reverts K2's Armour grid size for SE. more on line 66
@@ -53,7 +54,7 @@ end
 if mods["PowerAndArmor"] then
 	SetGridSize("PaA-power-armor-mk3-grid", "pa-mk3")
 	SetGridSize("PaA-power-armor-mk4-grid", "pa-mk4")
-
+-- crash here if mods
 	SetInvSize("pa-mk3", "cas-")
 	SetInvSize("pa-mk4", "cas-")
 end
@@ -83,7 +84,7 @@ if mods["custom_power_armor_fix"] then
 	SetInvSize("custom-power-armor-mk3", "cas-")
 	SetInvSize("custom-power-armor-mk4", "cas-")
 	SetInvSize("custom-power-armor-mk5", "cas-")
-	end
+end
 
 if mods["space-exploration"] then
 	SetGridSize("se-thruster-suit-grid", "se-thruster-suit")
@@ -95,6 +96,7 @@ if mods["space-exploration"] then
 	SetInvSize("se-thruster-suit-2", "cas-")
 	SetInvSize("se-thruster-suit-3", "cas-")
 	SetInvSize("se-thruster-suit-4", "cas-")
+end
 
 if mods["Krastorio2"] then
 		SetGridSize("mk3-armor-grid", "mk3-armor")
