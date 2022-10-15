5498fe782225c68e160f0bc20b7ebf9faa207f30
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2022-10-15 17:56:17 +0200

    MK10 mod progress

--- control.lua
+++ control.lua
@@ -1,2 +1,12 @@
 --function on
     --defines.events.on_player_armor_inventory_changed
+
+
+    local function on_player_joined_game(event)
+        local player = game.get_player(event.player_index)
+        if settings.blablavalue == "something" then
+          player.unlock_achievement("achievement_name")
+        end
+      end
+      
+      script.on_event(defines.events.on_player_joined_game, on_player_joined_game)
