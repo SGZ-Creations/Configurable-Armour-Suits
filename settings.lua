b3f2c1b0b654c23a753e29934d57d63b11dbd510
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2024-11-20 14:51:30 +0100

    Mech Armour mod

--- settings.lua
+++ settings.lua
@@ -1323,6 +1323,73 @@ if mods["space-age"] and mods["Power-Armour-Replacer"] then
     })
 end
 --------------------------------------------------------------------------------------------------
+--Mech Mod from SA 
+if mods["mech-armor"] and not mods["space-age"] then
+    data:extend({
+        {
+            type = "int-setting",
+            name = "mech-armor-grid-size-h",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 60,
+            default_value = 30,
+            order = "1Aal"
+        },
+        {
+            type = "int-setting",
+            name = "mech-armor-grid-size-w",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 60,
+            default_value = 30,
+            order = "1Aam"
+        },
+        --Inventory
+        {
+            type = "int-setting",
+            name = "sa-cas-mech-armor-inv-size",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 1500,
+            default_value = 150,
+            order = "2Aaf"
+        },
+    })
+end
+
+if mods["mech-armor"] and mods["Power-Armour-Replacer"] and not mods["space-age"] then
+    data:extend({
+        {
+            type = "int-setting",
+            name = "mech-armor-grid-size-h",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 600,
+            default_value = 60,
+            order = "1Aal"
+        },
+        {
+            type = "int-setting",
+            name = "mech-armor-grid-size-w",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 600,
+            default_value = 60,
+            order = "1Aam"
+        },
+        --Inventory
+        {
+            type = "int-setting",
+            name = "sa-cas-mech-armor-inv-size",
+            setting_type = "startup",
+            minimum_value = 0,
+            maximum_value = 3000,
+            default_value = 300,
+            order = "2Aaf"
+        },
+    })
+end
+--------------------------------------------------------------------------------------------------
 --Vanilla
 data:extend({
     {
