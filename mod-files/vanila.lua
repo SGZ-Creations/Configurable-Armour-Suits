3bf3457a38b8223d21717637ac181de2adef8b4e
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-02-12 13:19:13 +0100

    RE-implemented deleted code for vanilla light,heavy armours

--- mod-files/vanila.lua
+++ mod-files/vanila.lua
@@ -1,6 +1,5 @@
 local inv_grid = require("folder-of-functions.local-functions")
 
-
 inv_grid.SetGridSize("small-equipment-grid", "modular-armor")
 inv_grid.SetGridSize("medium-equipment-grid", "power-armor")
 inv_grid.SetGridSize("large-equipment-grid", "power-armor-mk2")
@@ -10,3 +9,22 @@ inv_grid.SetInvSize("heavy-armor", "v-cas-")
 inv_grid.SetInvSize("modular-armor", "v-cas-")
 inv_grid.SetInvSize("power-armor", "v-cas-")
 inv_grid.SetInvSize("power-armor-mk2", "v-cas-")
+
+data:extend({
+	{
+		type = "equipment-grid",
+		name = "equipment-grid-light",
+		height = settings.startup["light-grid-h"].value,
+		width = settings.startup["light-grid-w"].value,
+		equipment_categories = {"armor"}
+	},
+	{
+		type = "equipment-grid",
+		name = "equipment-grid-heavy",
+		height = settings.startup["heavy-grid-h"].value,
+		width = settings.startup["heavy-grid-w"].value,
+		equipment_categories = {"armor"}
+	},
+})
+data.raw["armor"]["light-armor"].equipment_grid = "equipment-grid-light"
+data.raw["armor"]["heavy-armor"].equipment_grid = "equipment-grid-heavy"
