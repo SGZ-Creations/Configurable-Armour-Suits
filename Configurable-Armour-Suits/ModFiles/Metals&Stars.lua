60af30f7e5bacd8c6e6c99556f2f8b6d227137d4
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-05 02:02:46 +0100

    local

--- Configurable-Armour-Suits/ModFiles/Metals&Stars.lua
+++ Configurable-Armour-Suits/ModFiles/Metals&Stars.lua
@@ -1,5 +1,8 @@
 local inv_grid = require("folder-of-functions.local-functions")
+---@class ArmorPrototype
 local Armour = data.raw["armor"]
+---@class LuaSettings
+local SS = settings.startup
 
 if mods["metal-and-stars"] and not mods["PARs-SpaceAge-Extension"] then
 	--inv_grid.SetGridSize("medium-equipment-grid", "MetalStarsMechSuit") --Currently Broken waiting on answer
@@ -10,8 +13,8 @@ if mods["metal-and-stars"] and not mods["PARs-SpaceAge-Extension"] then
 		{
 			type = "equipment-grid",
 			name = "MSMS-grid",
-			height = settings.startup["MSMS-grid-h"].value,
-			width = settings.startup["MSMS-grid-w"].value,
+			height = SS["MSMS-grid-h"].value,
+			width = SS["MSMS-grid-w"].value,
 			equipment_categories = {"armor"}
 		},
 	})
