8198c6df2ad40f661d87b3c5bcea15d34edc81fe
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-02-11 06:28:12 +0100

    Implemented Tiny2 fix dirrectly in to C.A.S

--- Configurable-Armour-Suits/ModFiles/TinyStart2.lua
+++ Configurable-Armour-Suits/ModFiles/TinyStart2.lua
@@ -4,15 +4,41 @@ local Armour = data.raw["armor"]
 ---@class LuaSettings
 local SS = settings.startup
 
-
 if mods["TinyStart-2"] then
-	inv_grid.SetGridSize("tiny-equipment-grid-0", "Tiny-0")
-	inv_grid.SetGridSize("tiny-equipment-grid-1", "Tiny-1")
-	inv_grid.SetGridSize("tiny-equipment-grid-2", "Tiny-2")
 
 	inv_grid.SetInvSize("tiny-armor-mk0", "Cas-Tiny0-")
 	inv_grid.SetInvSize("tiny-armor-mk1", "Cas-Tiny1-")
 	inv_grid.SetInvSize("tiny-armor-mk2", "Cas-Tiny2-")
+
+	Armour["tiny-armor-mk0"].equipment_grid = nil
+	Armour["tiny-armor-mk1"].equipment_grid = nil
+	Armour["tiny-armor-mk2"].equipment_grid = nil
+	data:extend({
+		{
+			type = "equipment-grid",
+			name = "tiny-equipment-grid-0",
+			height = SS["Tiny-0-grid-size-h"].value,
+			width = SS["Tiny-0-grid-size-w"].value,
+			equipment_categories = {"armor"}
+		},
+		{
+			type = "equipment-grid",
+			name = "tiny-equipment-grid-1",
+			height = SS["Tiny-1-grid-size-h"].value,
+			width = SS["Tiny-1-grid-size-w"].value,
+			equipment_categories = {"armor"}
+		},
+		{
+			type = "equipment-grid",
+			name = "tiny-equipment-grid-2",
+			height = SS["Tiny-2-grid-size-h"].value,
+			width = SS["Tiny-2-grid-size-w"].value,
+			equipment_categories = {"armor"}
+		},
+	})
+	Armour["tiny-armor-mk0"].equipment_grid = "tiny-equipment-grid-0"
+	Armour["tiny-armor-mk1"].equipment_grid = "tiny-equipment-grid-1"
+	Armour["tiny-armor-mk2"].equipment_grid = "tiny-equipment-grid-2"
 	--log("MainCode-Dependency")
 end
 --log("MainCode")
